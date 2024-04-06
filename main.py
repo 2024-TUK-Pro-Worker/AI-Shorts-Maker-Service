984b2f2838d9b015918e5c676cc690d7c24017c8
dawun <dawun@lacy.co.kr>
2024-04-06 16:00:10 +0900

    배경 음악 추가 (#테스트 제거)

--- main.py
+++ main.py
@@ -83,14 +83,14 @@ def makeShorts():
     global title
     global resourcePath
 
-    video_list = sorted(os.listdir(f"{resourcePath}/Video/{title}"))
-    video_clips = []
-    for video in video_list:
-        video_clip = VideoFileClip(f"{resourcePath}/Video/{title}/{video}")
-        video_clips.append(video_clip)
-    final_video_clip = concatenate_videoclips(video_clips)
-    final_video_path = f"{resourcePath}/Upload/tmp/{title}.mp4"
-    final_video_clip.write_videofile(final_video_path)
+    videoList = sorted(os.listdir(f"{resourcePath}/Video/{title}"))
+    videoClips = []
+    for video in videoList:
+        videoClip = VideoFileClip(f"{resourcePath}/Video/{title}/{video}")
+        videoClips.append(videoClip)
+    finalVideoClip = concatenate_videoclips(videoClips)
+    finalVideoPath = f"{resourcePath}/Upload/tmp/{title}.mp4"
+    finalVideoClip.write_videofile(finalVideoPath)
 
 
 def appendBgm():
@@ -102,23 +102,30 @@ def appendBgm():
 
     # 비디오 파일과 새 오디오 파일 불러오기
     video = VideoFileClip(f"{resourcePath}/Upload/tmp/{title}.mp4")
-    additional_audio = AudioFileClip(f"{resourcePath}/Bgm/{bgm}")
+    videoRange = int(video.duration) * 1000
+
+    # 영상 길이에 맞춰 브금 길이 자르기
+    bgmAudio = AudioSegment.from_file(f"{resourcePath}/Bgm/{bgm}")
+    trimmedAudio = bgmAudio[0:videoRange]
+    trimmedAudio.export(f"{resourcePath}/Audio/{title}/bgm.mp3", format="mp3")
+
+    additionalAudio = AudioFileClip(f"{resourcePath}/Audio/{title}/bgm.mp3")
 
     # 기존 오디오와 새 오디오의 볼륨 조정(1.0이 기존 볼륨 크기 기준)
-    original_audio = video.audio.volumex(2.0)
-    additional_audio = additional_audio.volumex(0.3)
+    originalAudio = video.audio.volumex(6.0)
+    additionalAudio = additionalAudio.volumex(0.2)
 
     # 조정된 볼륨으로 오디오 결합
-    combined_audio = CompositeAudioClip([original_audio, additional_audio])
+    combinedAudio = CompositeAudioClip([originalAudio, additionalAudio])
 
     # 비디오의 오디오를 결합된 오디오로 교체
-    video = video.set_audio(combined_audio)
+    video = video.set_audio(combinedAudio)
 
     # 결과 비디오 파일 저장(코덱: mp4 기준)
     video.write_videofile(f"{resourcePath}/Upload/{title}.mp4")
 
 
-def get_authenticated_service():
+def getAuthenticatedService():
     # OAuth 2.0 인증 흐름 초기화
     flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
     credentials = flow.run_local_server()
@@ -127,10 +134,10 @@ def get_authenticated_service():
     return build('youtube', 'v3', credentials=credentials)
 
 
-def upload_video(youtube, video_path, title, description, tags):
+def uploadVideo(youtube, videoPath, title, description, tags):
     try:
         # 동영상 업로드 정보 설정
-        request_body = {
+        requestBody = {
             'snippet': {
                 'title': title,
                 'description': description,
@@ -142,15 +149,15 @@ def upload_video(youtube, video_path, title, description, tags):
         }
 
         # 동영상 업로드 요청 생성
-        media = MediaFileUpload(video_path)
-        insert_request = youtube.videos().insert(
+        media = MediaFileUpload(videoPath)
+        insertRequest = youtube.videos().insert(
             part='snippet,status',
-            body=request_body,
+            body=requestBody,
             media_body=media
         )
 
         # 동영상 업로드 실행
-        response = insert_request.execute()
+        response = insertRequest.execute()
 
         # 업로드 성공 시 동영상 ID 반환
         if 'id' in response:
@@ -167,16 +174,16 @@ def youtubeUpload():
     global resourcePath
 
     # OAuth 2.0 인증 및 YouTube 서비스 생성
-    youtube = get_authenticated_service()
+    youtube = getAuthenticatedService()
 
     # 업로드할 동영상 파일 경로 설정
-    video_path = f"{resourcePath}/Upload/{title}.mp4"
+    videoPath = f"{resourcePath}/Upload/{title}.mp4"
 
     # 업로드할 동영상 정보 설정
     description = title
     tags = ['생성형AI', 'AIShorts']
 
-    upload_video(youtube, video_path, title, description, tags)
+    uploadVideo(youtube, videoPath, title, description, tags)
 
 
 def removeResource():
